b3d975022d5004c986ff409f66c9dc09692b4272
NoHomey <ivo.stratev.tues@gmail.com>
2021-03-01 17:31:45 +0200

    Adding validator helper package

---
+++ misc/go.mod
@@ -0,0 +1,5 @@
+module github.com/NoHomey/chaos-go-camp-proj/misc
+
+go 1.15
+
+require github.com/go-playground/validator/v10 v10.4.1 // indirect
---
+++ misc/validator/validator.go
@@ -0,0 +1,14 @@
+package validator
+
+import lib "github.com/go-playground/validator/v10"
+
+//Fields extracts Field from the given ValidationErrors list.
+func Fields(errs lib.ValidationErrors) []string {
+	res := make([]string, len(errs))
+	i := 0
+	for _, err := range errs {
+		res[i] = err.Field()
+		i++
+	}
+	return res
+}

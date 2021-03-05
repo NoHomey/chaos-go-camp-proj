413cec81e42cd90d84af5db35ea742d8cf0a12ee
NoHomey <ivo.stratev.tues@gmail.com>
2021-03-05 17:54:34 +0200

    Addig logging to the error handler

--- expose/errhandler/errhandler.go
+++ expose/errhandler/errhandler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/NoHomey/chaos-go-camp-proj/ctxerr"
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 //Error is the type of the error data.
@@ -14,31 +15,40 @@ type Error struct {
 	Data    interface{} `json:"data,omitempty"`
 }
 
-//Handler is the error handler.
-func Handler(ctx *fiber.Ctx, err error) error {
-	code := fiber.StatusBadRequest
-	var send Error
-	if e, ok := err.(ctxerr.Error); ok {
-		if coder, cOk := e.(interface{ HttpStatusCode() int }); cOk {
-			code = coder.HttpStatusCode()
+//Handler is the error handler creator.
+func Handler(logger *zap.Logger) func(ctx *fiber.Ctx, err error) error {
+	return func(ctx *fiber.Ctx, err error) error {
+		code := fiber.StatusBadRequest
+		var send Error
+		if e, ok := err.(ctxerr.Error); ok {
+			if coder, cOk := e.(interface{ HttpStatusCode() int }); cOk {
+				code = coder.HttpStatusCode()
+			} else {
+				code = statusCodeFromCtxError(e)
+			}
+			errCtx := e.Context()
+			send.Message = e.Text()
+			send.Name = errCtx.Name
+			send.Data = errCtx.Data
 		} else {
-			code = statusCodeFromCtxError(e)
+			if e, ok := err.(*fiber.Error); ok {
+				code = e.Code
+				send.Message = e.Message
+				send.Name = "fiber"
+			} else {
+				send.Message = "Unknown error"
+				send.Name = "unknown"
+			}
 		}
-		errCtx := e.Context()
-		send.Message = e.Text()
-		send.Name = errCtx.Name
-		send.Data = errCtx.Data
-	} else {
-		if e, ok := err.(*fiber.Error); ok {
-			code = e.Code
-			send.Message = e.Message
-			send.Name = "fiber"
-		} else {
-			send.Message = "Unknown error"
-			send.Name = "unknown"
-		}
+		logger.Info(
+			"Sending error",
+			zap.String("name", send.Name),
+			zap.String("message", send.Message),
+			zap.Reflect("data", send.Data),
+			zap.Reflect("status", code),
+		)
+		return ctx.Status(code).JSON(fiber.Map{"error": send})
 	}
-	return ctx.Status(code).JSON(fiber.Map{"error": send})
 }
 
 func statusCodeFromCtxError(err ctxerr.Error) int {

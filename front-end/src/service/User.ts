73e94d03a9c1cf8d0bbb6a079968d7bddc94b262
NoHomey <ivo.stratev.tues@gmail.com>
2021-03-10 16:56:55 +0200

    Adding User service

---
+++ front-end/src/service/User.ts
@@ -0,0 +1,148 @@
+import { Response, Consume, Make } from "../response";
+
+export interface Service {
+    Reload(): null | Response<User>
+    SignUp(data: SignUpData): Response<void>
+    SignIn(data: SignInData): Response<User>
+    SignOut(): Response<void>
+    Access(): Response<User>
+}
+
+export type SignUpData = {
+    name: string
+    email: string
+    password: string
+}
+
+export type SignInData = {
+    email: string
+    password: string
+}
+
+export type User = {
+    name: string
+    email: string
+}
+
+class Impl implements Service {
+    private refreshToken: null | string
+    private accessSyncToken: null | string
+
+    public constructor() {
+        this.refreshToken = null
+        this.accessSyncToken = null
+    }
+
+    public Reload(): null | Response<User> {
+        this.refreshToken = localStorage.getItem(refreshTokenKey)
+        if(!this.refreshToken) {
+            return null
+        }
+        return Consume<User>(
+            fetch(
+                url.access,
+                {
+                    cache: "no-cache",
+                    headers: {
+                        "Authorization": `PASETO ${this.refreshToken}`
+                    }
+                }
+            ),
+            this.consume.bind(this)
+        )
+    }
+
+    public SignUp(data: SignUpData): Response<void> {
+        return Make(
+            fetch(
+                url.signUp,
+                {
+                    method: "POST",
+                    cache: "no-cache",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                }
+            )
+        )
+    }
+
+    public SignIn(data: SignInData): Response<User> {
+        return Consume<User>(
+            fetch(
+                url.signIn,
+                {
+                    method: "POST",
+                    cache: "no-cache",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                }
+            ),
+            res => {
+                this.accessSyncToken = res.accessSyncToken
+                this.refreshToken = res.refreshToken
+                localStorage.setItem(refreshTokenKey, this.refreshToken!)
+                return {
+                    name: res.name!,
+                    email: res.email!
+                }
+            }
+        )
+    }
+
+    public SignOut(): Response<void> {
+        return Consume<void>(
+            fetch(
+                url.signOut,
+                {
+                    method: "POST",
+                    cache: "no-cache",
+                    headers: {
+                        "Authorization": `PASETO ${this.refreshToken}`
+                    }
+                }
+            ),
+            () => {
+                this.refreshToken = null
+                this.accessSyncToken = null
+            }
+        )
+    }
+
+    public Access(): Response<User> {
+        return Consume<User>(
+            fetch(
+                url.access,
+                {
+                    cache: "no-cache",
+                    headers: {
+                        "Authorization": `PASETO ${this.refreshToken}`
+                    }
+                }
+            ),
+            this.consume.bind(this)
+        )
+    }
+
+    private consume(res: any): User {
+        this.accessSyncToken = res.accessSyncToken
+        return {
+            name: res.name!,
+            email: res.email!
+        }
+    }
+}
+
+export default Impl
+
+const url = {
+    signUp: "/user/sign-up",
+    signIn: "user/sign-in",
+    signOut: "/user/sign-out",
+    access: "/user/access"
+}
+
+const refreshTokenKey = "$__refresh-toke__$"
